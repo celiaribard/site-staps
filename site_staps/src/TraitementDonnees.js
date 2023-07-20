b68f7f0b8e46b2b5ada08effac9cee322a8c389d
celialali <celia.rdc31@gmail.com>
2023-07-20 14:28:29 +0200

    comment

--- site_staps/src/TraitementDonnees.js
+++ site_staps/src/TraitementDonnees.js
@@ -145,6 +145,7 @@ const getResumesDonneesSujets = (donneesPoussees) => {
     return resumesDonnneesSujets;
 }
 
+// le sujet sélectionné (inputId) est toujours affiché pour pouvoir être comparé
 const filtrerDonnees = (donneesAffichees, filtres, inputId) => {
         // const donneesFiltrees = donneesAffichees.filter((sujet) => {
         // return (
