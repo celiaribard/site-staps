dbabb7599482bbdba5c9e126803c243129dceaf1
celialali <celia.rdc31@gmail.com>
2023-08-23 12:23:34 +0200

    horizontal chart

--- site_staps/src/components/HistogrammesSport.jsx
+++ site_staps/src/components/HistogrammesSport.jsx
@@ -104,22 +104,28 @@ const HistogrammesSport = ({ resumeDonneesSujets, parametres, sports }) => {
     };
 
     const options = {
-        // indexAxis: 'y',
+        scales: {
+            x: {
+                title: {
+                    display: true,
+                    text: unites[selectedParam] ? `${colonnesRenommees[selectedParam]} (${unites[selectedParam]})` : colonnesRenommees[selectedParam]
+                }
+            }
+        },
+        indexAxis: 'y',
+        // Elements options apply to all of the options unless overridden in a dataset
+        // In this case, we are setting the border of each horizontal bar to be 2px wide
         // elements: {
         //     bar: {
         //         borderWidth: 2,
-        //     },
+        //     }
         // },
         // responsive: true,
         // plugins: {
         //     legend: {
         //         position: 'right',
         //     },
-        //     title: {
-        //         display: true,
-        //         text: 'Chart.js Horizontal Bar Chart',
-        //     },
-        // },
+        // }
     };
     // console.log(selectedParam);
     // console.log(selectedData);
@@ -139,8 +145,6 @@ const HistogrammesSport = ({ resumeDonneesSujets, parametres, sports }) => {
                     ))}
                 </select>
             </label>
-
-            <p>aaaa</p>
             <Bar data={data} options={options}></Bar>
         </div>
     )
